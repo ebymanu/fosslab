79a943ce6df11e445b7143f71e15b2fe1a5dcf6f
ebymanu <35956126+ebymanu@users.noreply.github.com>
2018-04-12 08:56:57 +0530

    Add files via upload

---
+++ perl/basicperl.pl
@@ -0,0 +1,22 @@
+#basics
+print "basic perl\n";
+#comments are used with hash
+
+
+#variable a being given a value of 10
+$a = 10;
+
+#printing value of variable
+print "a = $a\n";
+
+
+@array = (1,2,3,4);
+print "Full array : @array\n";
+print "Elements one by one : $array[0] $array[1] $array[2] $array[3]\n";
+
+#strings
+$multiline = "Abc
+def
+ghi";
+print "Perl has multiline strings : $multiline \n";
+
